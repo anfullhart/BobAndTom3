0dd6ea658a01dceb25a9501b35e58eb8a3a56b1f
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-06-08 22:08:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-import Home from "./pages/home";
+import Home from "./pages/login";
 import Entry from "./pages/entry";
 import AdvancedSearch from "./pages/advancedSearch";
 import AddBit from "./pages/addBit";
