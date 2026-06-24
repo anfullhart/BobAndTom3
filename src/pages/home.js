6d45625fd71594ce47ee98911772477ac1d13025
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-06-23 21:49:44 -0400

    Update links to point to production URLs

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,7 @@ const Home = () => {
     
     return(
       <div style={{paddingLeft: '25%', paddingTop: '100px'}}>
-            <Link to="/searchMedia"
+            <Link to="bobandtom3-production.up.railway.app/searchMedia"
                 onMouseEnter={e => e.target.style.background = "grey"}
                 onMouseLeave={e => e.target.style.background = "black"} 
                  style={{textDecoration: 'none', backgroundColor: 'black', size: '100px',color:'white', fontSize: '20px', padding: '10px 60px', cursor: 'pointer', borderRadius: '5px' }}>
@@ -17,7 +17,7 @@ const Home = () => {
             </Link>
             
             <span style={{padding:'20px'}}></span>
-            <Link to="/searchRunSheet"
+            <Link to="bobandtom3-production.up.railway.app/searchRunSheet"
                 onMouseEnter={e => e.target.style.background = "grey"}
                 onMouseLeave={e => e.target.style.background = "black"} 
                  style={{textDecoration: 'none', backgroundColor: 'black', size: '100px',color:'white', fontSize: '20px', padding: '10px 60px', cursor: 'pointer', borderRadius: '5px' }}>
