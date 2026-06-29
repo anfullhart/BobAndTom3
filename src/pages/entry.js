5cc5ff873135a3f9cc07eb279a650a0709f61346
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-06-29 13:46:47 -0400

    Increase top padding in entry.js to 250px

--- src/pages/entry.js
+++ src/pages/entry.js
@@ -29,7 +29,7 @@ const Entry = () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        paddingTop: "25px",
+        paddingTop: "250px",
         transform: "translateY(-20%)"
       }}
     >
