5cea3ce247b79b3cf0189881b0d7bcff709c6e75
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-06-29 13:43:47 -0400

    Adjust layout styles in entry.js
    Replace minHeight with paddingTop for layout adjustment.

--- src/pages/entry.js
+++ src/pages/entry.js
@@ -29,7 +29,7 @@ const Entry = () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        minHeight: "100vh",
+        paddingTop: "100px",
         transform: "translateY(-20%)"
       }}
     >
