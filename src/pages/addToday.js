21d053567e8c57105e1295595007a50cbedbb13f
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-07-30 08:13:51 -0400

    Refactor addToday.js layout and input handling

--- src/pages/addToday.js
+++ src/pages/addToday.js
@@ -88,111 +88,144 @@ const AddToday = () => {
     setVisibleRows((prev) => prev + 5);
   };
 
-  return (
-    <form>
-      {/* Date Box */}
-      <div
+ return (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      minHeight: "100vh",
+      padding: "20px",
+      boxSizing: "border-box",
+    }}
+  >
+    {/* Date Box */}
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        width: "100%",
+        maxWidth: "300px",
+        borderRadius: "10px",
+        border: "3px solid black",
+        textAlign: "center",
+        padding: "10px",
+        marginBottom: "20px",
+      }}
+    >
+      Run Sheet Date:
+      <input
         style={{
-          backgroundColor: "white",
-          color: "black",
-          width: "240px",
-          borderRadius: "10px",
-          border: "3px solid black",
           marginLeft: "10px",
-          marginTop: "10px",
-          textAlign: "center",
-          padding: "10px"
+          width: "120px",
         }}
-      >
-        Run Sheet Date:
-        <input
-          style={{ marginLeft: "5%", width: "120px" }}
-          value={logDate}
-          onChange={(e) => setLogDate(e.target.value)}
-        />
-      </div>
+        value={logDate}
+        onChange={(e) => setLogDate(e.target.value)}
+      />
+    </div>
 
-      {/* Main Container */}
+    {/* Main Container */}
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        fontSize: "15px",
+        padding: "20px",
+        borderRadius: "15px",
+        width: "100%",
+        maxWidth: "1000px",
+      }}
+    >
       <div
         style={{
-          backgroundColor: "black",
-          color: "white",
-          fontSize: "15px",
-          marginTop: "20px",
-          marginLeft: "18%",
-          padding: "20px",
-          borderRadius: "15px",
-          width: "1000px"
+          display: "grid",
+          gridTemplateColumns: "120px 1fr 200px",
+          gap: "15px",
+          marginBottom: "15px",
+          fontWeight: "bold",
         }}
       >
-        <label style={{ marginLeft: "10%" }}>Time</label>
-        <label style={{ marginLeft: "30%" }}>Description</label>
-        <label style={{ marginLeft: "30%" }}>Artist</label>
-
-        {rows.slice(0, visibleRows).map((row, index) => (
-          <div key={index}>
-            <input
-              type="text"
-              placeholder="00:00:00"
-              value={row.time}
-              onChange={(e) => updateRow(index, "time", e.target.value)}
-            />
-
-            <input
-              type="text"
-              placeholder="Description"
-              style={{ marginLeft: "40px", width: "400px" }}
-              value={row.description}
-              onChange={(e) => updateRow(index, "description", e.target.value)}
-            />
-
-            <select
-              style={{ marginLeft: "5px" }}
-              value={row.artist}
-              onChange={(e) => updateRow(index, "artist", e.target.value)}
-            >
-              <option value="">Select Artist</option>
-              {artistList.map((val) => (
-                <option key={val.ArtistID} value={val.ArtistID}>
-                  {val.Name}
-                </option>
-              ))}
-            </select>
-          </div>
-        ))}
-
-        {/* Buttons */}
-        <div>
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={submitRunSheet}
-            style={{ marginLeft: "55%", marginTop: "20px" }}
-          >
-            Submit Run Sheet
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={clearRunSheet}
-            style={{ marginLeft: "10px", marginTop: "20px" }}
-          >
-            Clear Run Sheet
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={addFiveRows}
-            style={{ marginLeft: "10px", marginTop: "20px" }}
-          >
-            Add 5 Rows
-          </button>
-        </div>
+        <div>Time</div>
+        <div>Description</div>
+        <div>Artist</div>
       </div>
-    </form>
-  );
+
+      {rows.slice(0, visibleRows).map((row, index) => (
+        <div
+          key={index}
+          style={{
+            display: "grid",
+            gridTemplateColumns: "120px 1fr 200px",
+            gap: "15px",
+            marginBottom: "10px",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="00:00:00"
+            value={row.time}
+            onChange={(e) => updateRow(index, "time", e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="Description"
+            value={row.description}
+            onChange={(e) => updateRow(index, "description", e.target.value)}
+          />
+
+          <select
+            value={row.artist}
+            onChange={(e) => updateRow(index, "artist", e.target.value)}
+          >
+            <option value="">Select Artist</option>
+            {artistList.map((val) => (
+              <option key={val.ArtistID} value={val.ArtistID}>
+                {val.Name}
+              </option>
+            ))}
+          </select>
+        </div>
+      ))}
+
+      {/* Buttons */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          flexWrap: "wrap",
+          gap: "10px",
+          marginTop: "20px",
+        }}
+      >
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={submitRunSheet}
+        >
+          Submit Run Sheet
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={clearRunSheet}
+        >
+          Clear Run Sheet
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={addFiveRows}
+        >
+          Add 5 Rows
+        </button>
+      </div>
+    </div>
+  </div>
+);
 };
 
 export default AddToday;
