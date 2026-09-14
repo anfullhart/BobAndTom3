87d6f21ce9cef866bd4e5d73f61f87ebffcdbaa8
anfullhart <110847113+anfullhart@users.noreply.github.com>
2026-09-14 14:56:27 -0400

    Fix casing of logResults.css import

--- src/pages/logResults.js
+++ src/pages/logResults.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Axios from "axios";
-import "./LogResults.css";
+import "./logResults.css";
 
 const API_URL =
   process.env.REACT_APP_API_URL ||
